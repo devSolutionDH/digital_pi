cda86909e68302c17e23f4e32a9dbf7aec0064cc
juliohdsv <ricksv92@gmail.com>
2022-07-20 11:12:59 -0300

    Sprint 3 - Criando bcryptjs, uuid e database json

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const {v4: uuid} = require('uuid');
+const bcrypt = require('bcryptjs');
+
+const usersPath = path.join(__dirname, '..', 'database', 'users.json');
+
 const authController = {
 
     loginScreen: (request, response) => {
@@ -29,13 +36,35 @@ const authController = {
 
     create: (request, response) => {
 
-        console.table(request.body);
+       const users = JSON.parse(fs.readFileSync(usersPath));
+       const {name, email, cpf, birthday, cep, cel, address, user, password, file} = request.body;
+       const passwordHash = bcrypt.hashSync(password);
+
+       const newUser = {
+            id: uuid(),
+            name: name,
+            email: email,
+            cpf: cpf,
+            birthday: birthday,
+            cep: cep,
+            cel: cel,
+            address: address,
+            user: user,
+            password: passwordHash,
+            file: file
+       };
+        
+       users.push(newUser);
+
+       fs.writeFileSync(usersPath, JSON.stringify(users));
+        console.log(users); 
 
         return response.render('login', { 
             title: 'Login - Dev Solution',
             cssType: '/css/login.css',
             picture: '/image/login.png'
         });
+        
     },
     
 };
